e9c1b8acbb4ff1e094d0ee3ab2a3d85cd21c8a5c
Fajar Manghayu Nuswantoro <fajar.nuswantoro@iykra.com>
2026-07-02 09:39:18 +0700

    update test inquiry

--- backend/check-inquiry.js
+++ backend/check-inquiry.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-const c = require('/home/segawon/apps/segawon_topup/backend/src/controllers/pascabayar.controller');
+const c = require('/home/segawon/apps/segawon_topup/backend/src/controllers/pascabayar.controller.js');
 
 console.log(typeof c.inquiry);
